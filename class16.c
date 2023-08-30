e2d39d0d8a4911cc9fd1246e5c91c582ca73f26d
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-08-30 12:09:43 +0600

    Update class16.c

--- class16.c
+++ class16.c
@@ -3,6 +3,6 @@ int main() {
 int i, j, k, result; //i=5,j=15,k=7
 scanf("%d%d%d", &i, &j, &k);
 result=(((i%5)*3)+(j*2-k)+(k%5)*i);
-printf("The result is %d", result);
+printf("The result is %d", result); //solve the equation ((i%5)*3)+(j*2-k)+(k%5)*i where j,k,i is inserted value
 return 0;
 }
