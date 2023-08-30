793010046a2c0eccc11c7cbec1ea945fee5a893c
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-08-30 12:06:01 +0600

    Update class15.c

--- class15.c
+++ class15.c
@@ -3,6 +3,6 @@ int main() {
 int a, b, result; //a=-11,b=3
 scanf("%d%d", &a, &b);
 result=((a/b)*3+(a%b));
-printf("The result is %d", result);
+printf("The result is %d", result); //solve (a/b)*3+(a%b) with respect to a and b integer
 return 0;
 }
