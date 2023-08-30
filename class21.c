ec112b34d5f100859c8476bcb111e57a3e42eec3
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-08-30 19:26:09 +0600

    Update class21.c

--- class21.c
+++ class21.c
@@ -2,6 +2,6 @@
 int main() {
 int a []={2,3,4,5,6};
 int sum=a[0]+a[1]+a[2]+a[3]+a[4];
-printf("The sum is %d without loop\n", sum);
+printf("The sum is %d without loop\n", sum); //sum array
 return 0;
 }
