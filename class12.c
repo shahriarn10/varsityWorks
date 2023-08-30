ebe7240a5ac34b84435f43f2203a5e9f5cdcd9e9
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-08-30 10:53:36 +0600

    Update class12.c

--- class12.c
+++ class12.c
@@ -6,7 +6,7 @@ int main() {
         for(col=1; col<=row; col++) {
             printf("*");
         }
-        printf("\n");
+        printf("\n"); //print 1 to 5 times '*'
     }
     return 0;
 }
