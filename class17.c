4af44c6f5eba34a9f35d3962584616c6176b8062
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-08-30 19:16:18 +0600

    Update class17.c

--- class17.c
+++ class17.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
 int a=10, b=20, x, y;
-x=++a;
-y=b++;
-printf("x=%d, a=%d\n", x, a);
-printf("y=%d, b=%d\n", y, b);
+x=++a; //immediate increment
+y=b++; //not immediate increment
+printf("x=%d, a=%d\n", x, a); 
+printf("y=%d, b=%d\n", y, b); 
 return 0;
 }
